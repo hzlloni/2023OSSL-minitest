38335a1d315e831ff91c1d0b69bbee8f82e04c11
hzlloni <jhw030329@gmail.com>
2023-04-23 23:16:17 +0900

    create saveData

--- manager.c
+++ manager.c
@@ -40,7 +40,10 @@ void saveData(Product p[], int count){
 
 	//중량 가격 제품명
 	fp= fopen("product.txt","wt");
-	
+	for(int i=0; i<count; i++){
+		if(p[i].weight==-1) continue;
+			fprintf(fp, "%-15s %3dg %4d원\n", p[i].name,p[i].weight, p[i].price);
+	}	
 	
 	fclose(fp);
 	printf("저장됨!\n");
@@ -51,12 +54,20 @@ void saveData(Product p[], int count){
 int loadData(Product *p){
 	int count=0;
 	FILE*fp;
-
+	fp = fopen("product.txt","rt");
+	if(fp ==NULL){
+		printf("=> 파일 없음\n");
+		return 0;
+	}
 	//파일 내용을 읽어와서 배열에 값 추가하기
-
-
-
-
+	fscanf(fp, "%s",p->name);
+        if(feof(fp)) {
+                return count;
+        }
+        fscanf(fp, "%dg", &p->weight);
+        fscanf(fp, "%d원", &p->price);
+			count++;
 	printf("=> 로딩 성공!\n");
+	fclose(fp);
 	return count;
 }
